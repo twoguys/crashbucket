2b09c3cf65c1955be1d09c626a2e73b05552a88b
Marshall Huss <mwhuss@gmail.com>
2010-08-19 09:55:04 -0400

    Added check to see if beta invite has already been added

--- app/controllers/beta_invites_controller.rb
+++ app/controllers/beta_invites_controller.rb
@@ -5,12 +5,19 @@ class BetaInvitesController < ApplicationController
   end
   
   def create
-    @beta_invite = BetaInvite.new(params[:beta_invite])
-    
-    if @beta_invite.save
-      redirect_to new_beta_invite_path, :notice => "We'll let you know!"
+        
+    if BetaInvite.find_by_email(params[:beta_invite][:email])
+      redirect_to new_beta_invite_path, :alert => "We already have your email, yo!"
     else
-      redirect_to new_beta_invite_path, :alert => "Check yo self! You forgot your email..."
+    
+      @beta_invite = BetaInvite.new(params[:beta_invite])
+    
+      if @beta_invite.save
+        redirect_to new_beta_invite_path, :notice => "We'll let you know!"
+      else
+        redirect_to new_beta_invite_path, :alert => "Check yo self! You forgot your email..."
+      end
+      
     end
   end
   
